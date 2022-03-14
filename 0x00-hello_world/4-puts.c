29700efb1e9e767dfe814ffe4a6b41307c650d2f
Samuel <samemson32@gmail.com>
2022-03-14 16:40:34 +0100

    My 4th task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <>[Dstdio.h>
+/**
+*
+[D[D[D[D[C*[B*[[C* Main - entry point
+ *
+ * Return: 0 after executing
+*/
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return(0)[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;[B[B[B[A[D[D[D[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D;
+}
