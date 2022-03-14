a55ee4b0dfa76d0abb52057b6164e928bebb6827
Samuel <samemson32@gmail.com>
2022-03-14 16:52:10 +0100

    My 4th task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <>[Dstdio.h>
+#include <stdio.h>
 /**
-*
-[D[D[D[D[C*[B*[[C* Main - entry point
+ *main - entry point
  *
- * Return: 0 after executing
-*/
-int main (void)
+ *Return: 0 after printing the function
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-return(0)[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;[B[B[B[A[D[D[D[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
